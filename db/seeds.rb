ab389ac01844d13f555565314f85b31e49354c4a
Toshiaki Ikeda <toshiaki.ikeda.study@gmail.com>
2017-05-05 09:22:45 +0000

    “seektest”

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,13 +17,12 @@
                 )
  end
 
-
-#n = 1
-#while n <= 100
-#  Blog.create(
-#    title: "あああ",
-#    content: "hoge",
-#    user_id: n
-#  )
-#  n = n + 1
-#end
+n = 1
+while n <= 100
+  Blog.create(
+    title: "あああ",
+    content: "hoge",
+    user_id: n
+  )
+  n = n + 1
+end
