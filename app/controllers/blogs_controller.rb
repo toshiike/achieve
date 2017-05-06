50dee42b8637df25948c490b1079d1a899ccb305
Toshiaki Ikeda <toshiaki.ikeda.study@gmail.com>
2017-05-06 08:56:11 +0000

    dive10

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,7 +18,7 @@ class BlogsController < ApplicationController
     @blog = Blog.new(blogs_params)
     @blog.user_id = current_user.id
     if @blog.save
-    redirect_to blogs_path,notice:"ブログを作成しました！"
+    redirect_to blogs_path,notice:"ブログを作成しました！" 
     else
     render'new'
     end
